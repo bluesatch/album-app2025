e563d3ae8fcfcfef0243ddf2244aee2ffa2ac0d5
bluesatch <satchwerk@gmail.com>
2025-11-03 14:41:34 -0600

    Trying to build queryAction helper

--- daos/common/daoCommon.js
+++ daos/common/daoCommon.js
@@ -1,7 +1,7 @@
 const connect = require('../../config/dbconfig')
+// const { queryAction } = require('../../helpers/queryAction')
 
 const daoCommon = {
-
     // create methods that will query the database
     findAll: (req, res, table)=> {
 
@@ -9,6 +9,7 @@ const daoCommon = {
         connect.query(
             `SELECT * FROM ${table};`,
             (error, rows)=> {
+                // queryAction(res, error, rows, table)
                 if (!error) {
                     if (rows.length === 1) {
                         res.json(...rows)
@@ -17,6 +18,49 @@ const daoCommon = {
                     }
                 } else {
                     console.log(`Dao Error: ${error}`)
+                    res.json({
+                        "message": 'error',
+                        'table': `${table}`,
+                        'error': error
+                    })
+                }
+            }
+        )
+    },
+    
+    findById: (res, table, id)=> {
+
+        connect.query(
+            `SELECT * FROM ${table} WHERE ${table}_id = ${id};`,
+            (error, rows)=> {
+                if (!error) {
+                    res.json(...rows)
+                } else {
+                    console.log(`DAO Error: ${error}`)
+                    res.json({
+                        "message": 'error',
+                        'table': `${table}`,
+                        'error': error
+                    })
+                }
+            }
+        )
+    },
+
+    sort: (res, table, sorter)=> {
+
+        connect.query(
+            `SELECT * FROM ${table} ORDER BY ${sorter};`,
+
+            (error, rows)=> {
+                if (!error) {
+                    if (rows.length == 1) {
+                        res.json(...rows)
+                    } else {
+                        res.json(rows)
+                    }
+                } else {
+                    console.log(`DAO Error: ${error}`)
                     res.json({
                         "message": 'error',
                         'table': `${table}`,

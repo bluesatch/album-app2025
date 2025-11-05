125232bbeed53aa34490aed18114c27f0e8686d2
bluesatch <satchwerk@gmail.com>
2025-11-05 14:36:04 -0600

    added create method

--- routes/api/bandRoutes.js
+++ routes/api/bandRoutes.js
@@ -20,4 +20,8 @@ router.get('/:id', (req, res)=> {
     dao.findById(res, dao.table, req.params.id)
 })
 
+router.post('/create', (req, res)=> {
+    dao.create(req, res, dao.table)
+})
+
 module.exports = router
